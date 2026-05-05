31658eb6ffcf057dc0194074fa4a7a0492787875
chakerox <chakerox@gmail.com>
2026-05-05 21:23:24 +0700

    membuat class kendaraan dan membuat constructor

--- TugasPraktikum.cpp
+++ TugasPraktikum.cpp
@@ -1,3 +1,36 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Kendaraan {
+private:
+    int id;
+    string nama;
+
+public:
+    Kendaraan() {
+        id = 0;
+        nama = "Tidak diketahui";
+    }
+
+    Kendaraan(int i) {
+        id = i;
+        nama = "Tidak diketahui";
+    }
+
+    Kendaraan(string n) {
+        id = 0;
+        nama = n;
+    }
+
+    Kendaraan(int i, string n) {
+        id = i;
+        nama = n;
+    }
+
+    void tampilkan() {
+        cout << "ID Kendaraan   : " << id << endl;
+        cout << "Nama Kendaraan : " << nama << endl;
+        cout << "---------------------------" << endl;
+    }
+};
