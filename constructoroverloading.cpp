a301e81d2c042931ed694f5ffb3288cd7d50dac4
chakerox <chakerox@gmail.com>
2026-05-05 21:16:55 +0700

    membuat prosedur cetak untuk mengeluarkan hasil

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -29,3 +29,8 @@ mahasiswa :: mahasiswa(int iNim, string iNama) {
     nim = iNim; 
     nama = iNama;
 }
+
+void mahasiswa :: cetak() {
+    cout << endl << "NIM = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+}
